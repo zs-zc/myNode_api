2c0c5c7a4e08234e790685c664e7a38ba5cb2648
zs-zc <3011121187@qq.com>
2022-07-19 22:32:24 +0800

    fix:封装报错信息

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const app = express()
 // 导入中间件
 const cors = require("cors")
 app.use(cors())
-
 // 解析表单数据
 app.use(express.urlencoded({ extended: false }))  // 解析数据表单要在 路由之前
+// 在路由之前封装res.cc函数
+app.use((req, res, next) => {
+    res.cc = function (err, status = 1) {
+        res.send({
+            status,
+            message: err instanceof Error ? err.message : err
+        })
+    }
+    next()
+})
+
 // 导入路由
 const userRouter = require("./router/user")
 app.use('/api', userRouter)
--- router_handle/user.js
+++ router_handle/user.js
@@ -3,26 +3,27 @@ const db = require("../db/index")
 const bcrypt = require("bcryptjs")
 exports.reguser = (req, res) => {
     const userInfo = req.body
+    console.log(userInfo, 'userInfo');
     if (!userInfo.username || !userInfo.password) {
-        return res.send({ status: 1, message: "用户名或密码不合法" })
+        return res.cc("用户名或密码不合法")
     }
     // 定义sql语句
     const sqlStr = 'select * from ev_users where username=?'
     db.query(sqlStr, userInfo.username, (err, results) => {
         if (err) {
-            return res.send({ ststus: 1, message: err.message })
+            return res.cc(err)
         }
         if (results.length > 0) {
-            return res.send({ ststus: 1, message: '用户名已被占用，请切换' })
+            return res.cc('用户名已被占用，请切换')
 
         }
         //调用bcrypt.hashSync进行加密
         userInfo.password = bcrypt.hashSync(userInfo.password, 10)
         const sql = 'insert into ev_users set ?'
         db.query(sql, { username: userInfo.username, password: userInfo.password }, (err, results) => {
-            if (err) return res.send({ status: 1, message: err.message })
-            if (results.affectedRows !== 1) return res.send({ status: 1, message: "注册用户失败，请稍后再试" })
-            res.send({ status: 0, message: "注册成功" })
+            if (err) return res.cc(err)
+            if (results.affectedRows !== 1) return res.cc("注册用户失败，请稍后再试")
+            res.send("注册成功", 0)
         })
     })
 }
